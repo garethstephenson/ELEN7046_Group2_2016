64e79956bf31ac67d9d3c9ee7d38e5b9ff816b84
Matsobane Khwinana <Matsobane.Khwinana@momentum.co.za>
2016-05-19 11:01:48 +0200

    MK: Added 5 Trump Tweets prior to New York Primary test case

--- twitter/java-data-extraction-service/src/test/java/za/ac/wits/cpd/elen7046/group2/ExtractAndStoreTweetsTest.java
+++ twitter/java-data-extraction-service/src/test/java/za/ac/wits/cpd/elen7046/group2/ExtractAndStoreTweetsTest.java
@@ -1,5 +1,8 @@
 package za.ac.wits.cpd.elen7046.group2;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,7 +27,7 @@ public class ExtractAndStoreTweetsTest {
     }
 
     @Test
-    public void testExtractAndStore() {
+    public void testExtractAndStoreById() {
         //Given
         Long twitterId = 729674502339055617L;
         
@@ -36,5 +39,38 @@ public class ExtractAndStoreTweetsTest {
         //Then
         assertTrue(Objects.equals(dbTweet.getTwitterId(), twitterId));
     }
+    
+    
+    @Test
+    public void testExtractAndStoreTrumpPriorToNYPrimary() {
+        //Given
+        final String hashtag = "MakeAmericaGreatAgain";
+        final Map<String, String> options = fiveTweetsBeforeNewYorkOptions();
 
+        //When
+        List<Tweet> tweetData = this.dataExtractor.extractHashtagTweets(hashtag, options);
+        for (Tweet tweet : tweetData) {
+            this.persistenceManager.persist(tweet);
+            Tweet dbTweet = persistenceManager.findByTwitterId(tweet.getTwitterId());
+            
+            //Then 
+            assertTrue(Objects.equals(dbTweet.getTwitterId(), tweet.getTwitterId()));
+        }
+    }
+
+    private Map<String, String> fiveTweetsBeforeNewYorkOptions() {
+        //https://twitter.com/search?q=%23MakeAmericaGreatAgain%20since%3A2016-03-19%20until%3A2016-04-19
+        final Map<String, String> options = new HashMap<>();
+        options.put(COUNT, FIVE);
+        options.put(SINCE, NY_PRIMARY_THIRTY_DAYS_PRIOR);
+        options.put(UNTIL, NY_PRIMARY_2016);
+        return options;
+    }
+    
+    private static final String NY_PRIMARY_THIRTY_DAYS_PRIOR = "2016-03-19";
+    private static final String NY_PRIMARY_2016 = "2016-04-19";
+    private static final String COUNT = "count";
+    private static final String SINCE = "since";
+    private static final String UNTIL = "until";
+    private static final String FIVE = "5";
 }
