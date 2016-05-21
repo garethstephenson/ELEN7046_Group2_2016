e2a843e034f6fd28256dc0d5c15cd7794a9c1140
Matsobane Khwinana <Matsobane.Khwinana@momentum.co.za>
2016-05-21 21:22:27 +0200

    MK: Added the ability to lookup a default location if tweet location is invalid

--- twitter/java-data-extraction-service/src/main/java/za/ac/wits/cpd/service/bigdatavisual/LocationResolver.java
+++ twitter/java-data-extraction-service/src/main/java/za/ac/wits/cpd/service/bigdatavisual/LocationResolver.java
@@ -25,15 +25,7 @@ public class LocationResolver {
      */
     public static Coordinate getGeoCodes(String locationName) {
         try {
-            GeoApiContext context = new GeoApiContext().setApiKey(GOOGLE_API_KEY);
-            GeocodingResult[] results = GeocodingApi.geocode(context, locationName).await();
-
-            if (results != null && results.length > 0) {
-                GeocodingResult result = results[0];
-                Geometry geometry = result.geometry;
-                LatLng latlng = geometry.location;
-                return new Coordinate(latlng.lat, latlng.lng);
-            }
+            return lookupCoordinates(locationName);
         } catch (Exception ex) {
             log.log(Level.SEVERE, "Failed to lookup geo-location coordinates for " + locationName, ex);
         }
@@ -41,64 +33,35 @@ public class LocationResolver {
         return null;
     }
 
+    
+    public static Coordinate getGeoCodes(String locationName, String defaultLocationName) {
+        try {
+            final Coordinate coordinates = lookupCoordinates(locationName); 
+            if (coordinates!=null) 
+                return coordinates;
+            else
+                return lookupCoordinates(defaultLocationName);
+        } catch (Exception ex) {
+            log.log(Level.SEVERE, "Failed to lookup geo-location coordinates for " + locationName, ex);
+        }
+
+        return null;
+    }
+    
+    private static Coordinate lookupCoordinates(String locationName) throws Exception {
+        GeoApiContext context = new GeoApiContext().setApiKey(GOOGLE_API_KEY);
+        GeocodingResult[] results = GeocodingApi.geocode(context, locationName).await();
+        if (results != null && results.length > 0) {
+            GeocodingResult result = results[0];
+            log.severe(result.toString());
+            Geometry geometry = result.geometry;
+            LatLng coordinates = geometry.location;
+            return new Coordinate(coordinates.lat, coordinates.lng);
+        }
+        
+        return null;
+    }
+
     private static final String GOOGLE_API_KEY = "AIzaSyA3kPdcv9LNfthpEUHFkvh0vGvl8XpSdww";
-    
-    /*
-    {
-   "results" : [
-      {
-         "address_components" : [
-            {
-               "long_name" : "Texas",
-               "short_name" : "TX",
-               "types" : [ "administrative_area_level_1", "political" ]
-            },
-            {
-               "long_name" : "United States",
-               "short_name" : "US",
-               "types" : [ "country", "political" ]
-            }
-         ],
-         "formatted_address" : "Texas, USA",
-         "geometry" : {
-            "bounds" : {
-               "northeast" : {
-                  "lat" : 36.5007041,
-                  "lng" : -93.5080389
-               },
-               "southwest" : {
-                  "lat" : 25.8371638,
-                  "lng" : -106.6456461
-               }
-            },
-            "location" : {
-               "lat" : 31.9685988,
-               "lng" : -99.9018131
-            },
-            "location_type" : "APPROXIMATE",
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 36.5015087,
-                  "lng" : -93.5080389
-               },
-               "southwest" : {
-                  "lat" : 25.8371638,
-                  "lng" : -106.6456461
-               }
-            }
-         },
-         "partial_match" : true,
-         "place_id" : "ChIJSTKCCzZwQIYRPN4IGI8c6xY",
-         "types" : [ "administrative_area_level_1", "political" ]
-      }
-   ],
-   "status" : "OK"
-}
-    
-    
-    
-    
-    
-    
-    */
+
 }
