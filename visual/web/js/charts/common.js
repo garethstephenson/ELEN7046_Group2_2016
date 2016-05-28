6644e0135ef3ecf5f2929c014d4a5f95e0971ea3
dcloete@iqgroup.net <dcloete@iqgroup.net>
2016-05-28 16:42:51 +0200

    DC : Hourly Heat Map - Not complete

---
+++ visual/web/js/charts/common.js
@@ -0,0 +1,53 @@
+/* 
+ * This file holds common 'static' functions shared betwee charts
+ */
+/* global d3 */
+var commonCharts = {
+  // Array Helpers
+  getIndexByValue: function (value, array) {
+    for (var index = 0; index < array.length; index++) {
+      if (array[index] === value) {
+        return index;
+      }
+    }
+    return -1;
+  },
+  // Date Helpers
+  getDatePartValue: function (dateString, datePart) {
+    //ref: https://github.com/d3/d3/wiki/Time-Formatting
+    var dateFormat = d3.time.format("%Y-%m-%d");
+    var printFormat = d3.time.format(datePart);
+    var date = dateFormat.parse(dateString);
+    return printFormat(date);
+  },
+  getHourValue: function (dateString) {
+    return commonCharts.getDatePartValue(dateString, "%H");
+  },
+  getDayValue: function (dateString) {
+    return commonCharts.getDatePartValue(dateString, "%d");
+  },
+  getMonthValue: function (dateString) {
+    return commonCharts.getDatePartValue(dateString, "%m");
+  },
+  getDateValue: function (dateString) {
+    return commonCharts.getDatePartValue(dateString, "%Y-%m-%d");
+  },
+  make2DigitNumberArray: function (start, end) {
+    var array = [];
+    for (var index = start; index <= end; index++) {
+      array.push(("0" + (index)).slice(-2));
+    }
+    return array;
+  },
+  // Category Color Helprs 
+  getCategoryColor: function (category, categoryColors) {
+    for (var index = 0; index < categoryColors.length; index++) {
+      var colorItem = categoryColors[index];
+      if (category.Category === colorItem.Category) {
+        return colorItem.Color;
+      }
+    }
+    return "#000000";
+  }
+};
+
