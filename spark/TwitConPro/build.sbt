a24951b1e6d157687b3826664334576e70d6e654
Gareth Stephenson <gareth.stephenson@gmail.com>
2016-06-06 22:21:01 +0200

    Able to count, still need to work on hours breakdown.

--- spark/TwitConPro/build.sbt
+++ spark/TwitConPro/build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.1"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.6.1"
+libraryDependencies += "org.apache.hadoop" % "hadoop-hdfs" % "2.6.4"
--- spark/TwitConPro/src/main/scala-2.11/org/TwitConPro/CategoryCountPerHour.scala
+++ spark/TwitConPro/src/main/scala-2.11/org/TwitConPro/CategoryCountPerHour.scala
@@ -1,5 +1,7 @@
 package org.TwitConPro
 
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs._
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -7,43 +9,59 @@ import org.apache.spark.{SparkConf, SparkContext}
   * Created by Gareth on 2016/06/06.
   */
 object CategoryCountPerHour {
-  def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit = {
 
-    val sparkConfig = new SparkConf()
-    sparkConfig.setAppName("Category Count Per Hour")
+        val sparkConfig = new SparkConf()
+        sparkConfig.setAppName("Category Count Per Hour")
 
-    val sparkContext = new SparkContext(sparkConfig)
+        val sparkContext = new SparkContext(sparkConfig)
 
-    var inputPath = ""
-    if (args.length > 0)
-      inputPath = args(0)
-    else {
-      println("An invalid source path was supplied.")
-      printUsage()
-      return
+        var inputPath = ""
+        if (args.length > 0)
+            inputPath = args(0)
+        else {
+            println("An invalid source path was supplied.")
+            printUsage()
+            return
+        }
+
+        var categories = Array[String]()
+        if (args.length > 1) {
+            categories = args(1).split(",")
+            val catLen = categories.length
+            println(s"\tFound $catLen categories")
+        }
+        else {
+            println("Categories must be supplied.")
+            printUsage()
+            return
+        }
+
+        val sqlContext = new SQLContext(sparkContext)
+        val tweets = sqlContext.read.json(inputPath)
+
+        for (category <- categories) {
+            println(s"\tWorking on category '$category'")
+
+            tweets.registerTempTable("tweets")
+
+            val count = sqlContext.sql(s"SELECT COUNT(*) FROM tweets WHERE tweetText LIKE '%$category%'")
+            count.write.json(s"/data/$category-count.out")
+            merge(s"/data/$category-count.out", s"/data/$category-count.json")
+        }
+
+        sparkContext.stop()
     }
 
-    val categories = Set()
-    if (args.length > 1)
-      categories.++(args(1).split(","))
-    else {
-      println("Categories must be supplied.")
-      printUsage()
-      return
+    def merge(srcPath: String, dstPath: String): Unit = {
+        val hadoopConfig = new Configuration()
+        val hdfs = FileSystem.get(hadoopConfig)
+        FileUtil.copyMerge(hdfs, new Path(srcPath), hdfs, new Path(dstPath), false, hadoopConfig, null)
+        FileUtil.fullyDelete(hdfs, new Path(srcPath))
     }
 
-    val broadcastCategories = sparkContext.broadcast(categories)
-
-    val sqlContext = new SQLContext(sparkContext)
-    val records = sqlContext.read.json(inputPath)
-
-    
-
-    sparkContext.stop()
-  }
-
-  private def printUsage(): Unit = {
-    println("Usage: CountPerCategory [path] [categories]")
-    println("Eg: CountPerCategory /path/to/file category1[,category2[,category3]]")
-  }
+    private def printUsage(): Unit = {
+        println("Usage: CategoryCountPerHour [path] [categories]")
+        println("Eg: CategoryCountPerHour /path/to/file category1[,category2[,category3]]")
+    }
 }
