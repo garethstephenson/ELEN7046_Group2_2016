476985750b8ebcd6b8344d6c77d6f207f2b25337
Gareth Stephenson <Gareth Stephenson>
2016-06-06 14:01:29 +0200

    Added Spark dependency to project

--- spark/TwitConPro/build.sbt
+++ spark/TwitConPro/build.sbt
@@ -3,3 +3,5 @@ name := "TwitConPro"
 version := "1.0"
 
 scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.1"
--- spark/TwitConPro/src/main/scala-2.11/org/TwitConPro/CategoryCountPerHour.scala
+++ spark/TwitConPro/src/main/scala-2.11/org/TwitConPro/CategoryCountPerHour.scala
@@ -1,8 +1,20 @@
 package org.TwitConPro
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by Gareth on 2016/06/06.
   */
 object CategoryCountPerHour {
+  def main(args: Array[String]): Unit = {
 
+    val sparkConfig = new SparkConf()
+    sparkConfig.setAppName("Category Count Per Hour")
+
+    val sparkContext = new SparkContext(sparkConfig)
+
+
+
+    sparkContext.stop()
+  }
 }
