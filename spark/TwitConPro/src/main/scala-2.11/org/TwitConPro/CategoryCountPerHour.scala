e695f1a40ef3bdeb627c36acddcb8e87381949fc
Gareth Stephenson <gareth.stephenson@gmail.com>
2016-06-06 15:22:45 +0200

    Added spark-sql dependency in order to read json files as DataFrames

--- spark/TwitConPro/src/main/scala-2.11/org/TwitConPro/CategoryCountPerHour.scala
+++ spark/TwitConPro/src/main/scala-2.11/org/TwitConPro/CategoryCountPerHour.scala
@@ -1,5 +1,6 @@
 package org.TwitConPro
 
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -13,8 +14,36 @@ object CategoryCountPerHour {
 
     val sparkContext = new SparkContext(sparkConfig)
 
+    var inputPath = ""
+    if (args.length > 0)
+      inputPath = args(0)
+    else {
+      println("An invalid source path was supplied.")
+      printUsage()
+      return
+    }
 
+    val categories = Set()
+    if (args.length > 1)
+      categories.++(args(1).split(","))
+    else {
+      println("Categories must be supplied.")
+      printUsage()
+      return
+    }
+
+    val broadcastCategories = sparkContext.broadcast(categories)
+
+    val sqlContext = new SQLContext(sparkContext)
+    val records = sqlContext.read.json(inputPath)
+
+    
 
     sparkContext.stop()
+  }
+
+  private def printUsage(): Unit = {
+    println("Usage: CountPerCategory [path] [categories]")
+    println("Eg: CountPerCategory /path/to/file category1[,category2[,category3]]")
   }
 }
