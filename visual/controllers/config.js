5ce795cec296455c09d9ca67e567ffcb163786a4
dcloete@iqgroup.net <dcloete@iqgroup.net>
2016-06-08 09:19:44 +0200

    DC : initial work for making UI change topics

--- visual/controllers/config.js
+++ visual/controllers/config.js
@@ -1,14 +1,56 @@
 var express = require('express');
 var router = express.Router();
 var Twig = require("twig");
-
+var fs = require("fs");
 // /config/*
 
 router.get('/', function (req, res) {
 
-  res.send('config');
-  res.end();
-  //res.render('index.html.twig', {});
+  getConfigData(function (configData) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(configData);
+  }, function () {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(null);
+  });
 });
+router.get('/topics', function (req, res) {
+
+  getConfigData(function (configData) {
+
+    getTopicsData(__dirname + configData.topicsPath, function (topicData) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(topicData);
+    }, function () {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(null);
+    });
+  }, function () {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(null);
+  });
+});
+
+function getConfigData(successCallBack, errorCallBack) {
+  fs.readFile(__dirname + "/../config/" + "dataConfig.json", 'utf8', function (error, data) {
+    if (error) {
+      console.log("Error reading file: %s", error);
+      errorCallBack(err);
+    }
+    var configData = JSON.parse(data);
+    successCallBack(configData);
+  });
+}
+
+function getTopicsData(topicsPath, successCallBack, errorCallBack) {
+  fs.readFile(topicsPath + "topics.json", 'utf8', function (error, data) {
+    if (error) {
+      console.log("Error reading file: %s", error);
+      errorCallBack(err);
+    }
+    var topicsData = JSON.parse(data);
+    successCallBack(topicsData);
+  });
+}
 
 module.exports = router;
