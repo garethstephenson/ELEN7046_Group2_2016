66bc2119b302be984e924026424ff721b37ddc46
Gareth Stephenson <gareth.stephenson@gmail.com>
2016-06-11 17:46:26 +0200

    json serliazation work in order to read tweet data supplied by Kyle

--- spark/TwitConPro/src/main/scala-2.11/org/TwitConPro/Tweet.scala
+++ spark/TwitConPro/src/main/scala-2.11/org/TwitConPro/Tweet.scala
@@ -1,6 +1,6 @@
 package org.TwitConPro
 
-import spray.json.DefaultJsonProtocol
+import spray.json.{DefaultJsonProtocol, JsNumber, JsString, JsValue, RootJsonFormat}
 
 /**
   * Created by Gareth on 2016/06/11.
@@ -13,13 +13,13 @@ case class Tweet(_id: String,
                  hashtags: Array[HashTag],
                  twitterID: String,
                  inReplyToName: String,
-                 inReplyToStatusID: Int,
-                 inReplyToUserID: Int,
+                 inReplyToStatusID: Either[String, Int],
+                 inReplyToUserID: Either[String, Int],
                  isRetweet: Boolean,
                  language: String,
                  place: String,
                  sensitive: Boolean,
-                 quotedStatusID: Int,
+                 quotedStatusID: Either[String, Int],
                  retweeted: Boolean,
                  retweetedCount: Int,
                  tweetText: String,
@@ -27,12 +27,36 @@ case class Tweet(_id: String,
 
 object TweetJsonProtocol extends DefaultJsonProtocol {
   implicit val hashTagFormat = jsonFormat(HashTag, "hashTag")
+
+
   implicit val tweetFormat = jsonFormat(Tweet, "_id", "createdBy", "createdAt", "coords", "favouriteCount", "hashtags",
     "twitterID", "inReplyToName", "inReplyToStatusID", "inReplyToUserID", "isRetweet", "language", "place", "sensitive",
     "quotedStatusID", "retweeted", "retweetedCount", "tweetText", "tweetURL")
-  /*  implicit object tweetFormat extends RootJsonFormat[Tweet] {
-      def read(json: JsValue): Tweet = json match {
-        case JsArray(Vector(JsString()))
+
+
+
+/*  implicit object tweetFormat extends RootJsonFormat[Tweet] {
+      def read(json: JsValue) = {
+        json.asJsObject.getFields("_id", "createdBy", "createdAt", "coords", "favouriteCount", "hashtags",
+          "twitterID", "inReplyToName", "inReplyToStatusID", "inReplyToUserID", "isRetweet", "language", "place", "sensitive",
+          "quotedStatusID", "retweeted", "retweetedCount", "tweetText", "tweetURL") match {
+          case JsNumber(inReplyToStatusID) => new Tweet(json.asJsObject.getFields("_id"),
+            json.asJsObject.getFields("createdBy"),
+            json.asJsObject.getFields("createdAt"),
+            json.asJsObject.getFields("_id"),
+            json.asJsObject.getFields("_id"),
+            json.asJsObject.getFields("_id"),
+            json.asJsObject.getFields("_id"),
+            json.asJsObject.getFields("_id"),
+            json.asJsObject.getFields("_id"),
+            json.asJsObject.getFields("_id"),
+            json.asJsObject.getFields("_id"),
+            json.asJsObject.getFields("_id"),
+            json.asJsObject.getFields("_id"),
+
+          )
+        }
+        case Seq(JsString())
       }
     }*/
 }
