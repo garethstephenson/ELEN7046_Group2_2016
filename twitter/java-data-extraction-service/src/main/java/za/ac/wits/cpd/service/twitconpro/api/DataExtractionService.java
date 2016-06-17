c12893d7862de223c8b14f6b158d499da8116ee9
Matsobane Khwinana <Matsobane.Khwinana@momentum.co.za>
2016-06-17 14:49:57 +0200

    MK: Added 'distribution of all tweets'and 'random hashtag tweets' use cases

--- twitter/java-data-extraction-service/src/main/java/za/ac/wits/cpd/service/twitconpro/api/DataExtractionService.java
+++ twitter/java-data-extraction-service/src/main/java/za/ac/wits/cpd/service/twitconpro/api/DataExtractionService.java
@@ -9,16 +9,18 @@ import java.util.Map;
  * @author Matsobane Khwinana (Matsobane.Khwinana@momentum.co.za)
  */
 public interface DataExtractionService {
+    
     public Tweet exactTweetById(Long id);
     public Tweet exactTweetByIdAndPersist(Long id);
     
-    public List<Tweet> extractTweetsByHashtag(String hashtag);
-    public List<Tweet> extractTweetsByHashtagAndPersist(String hashtag);
+    public List<Tweet> extractRandomTweetsByHashtag(String hashtag);
+    public List<Tweet> extractAndPersistRandomTweetsByHashtag(String hashtag);
     
-    public List<Tweet> extractTweetsByHashtag(String hashtag,int count);
-    public List<Tweet> extractTweetsByHashtagAndPersist(String hashtag,int count);
+    public List<Tweet> extractHistoryTweetsByHashtag(String hashtag,int count);
+    public List<Tweet> extractHistoryTweetsByHashtagAndPersist(String hashtag,int count);
     
-    public List<Tweet> extractTweetsByHashtags(List<String> hashtags, Map<String, String> options);
-    public List<Tweet> extractTweetsByHashtagsAndPersist(List<String> hashtags, Map<String, String> options);
+    public List<Tweet> extractHistoryTweetsByHashtags(List<String> hashtags, Map<String, String> options);
+    public List<Tweet> extractHistoryTweetsByHashtagsAndPersist(List<String> hashtags, Map<String, String> options);
 
+    public List<Tweet> distributeAllTweets();
 }
