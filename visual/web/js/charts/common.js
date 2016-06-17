8ccc5d46707a7b3e5c814a70f11978d346dd4ecd
dcloete@iqgroup.net <dcloete@iqgroup.net>
2016-06-17 20:18:56 +0200

    DC : added stream graphs

--- visual/web/js/charts/common.js
+++ visual/web/js/charts/common.js
@@ -63,5 +63,15 @@ var commonCharts = {
     }
     return maxCategoryItem;
   },
+  invertColor: function (hexTripletColor) {
+    var color = hexTripletColor;
+    color = color.substring(1);           // remove #
+    color = parseInt(color, 16);          // convert to integer
+    color = 0xFFFFFF ^ color;             // invert three bytes
+    color = color.toString(16);           // convert to hex
+    color = ("000000" + color).slice(-6); // pad with leading zeros
+    color = "#" + color;                  // prepend #
+    return color;
+  }
 };
 
