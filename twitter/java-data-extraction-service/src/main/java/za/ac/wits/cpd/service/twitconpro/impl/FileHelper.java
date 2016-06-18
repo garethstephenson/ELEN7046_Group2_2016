8018a4666f8c7b1d873f6d262bf8e396f6ca3cdb
Matsobane Khwinana <Matsobane.Khwinana@momentum.co.za>
2016-06-18 14:11:03 +0200

    MK: Overloaded the FileHelper.write to include the URI and FileName...

--- twitter/java-data-extraction-service/src/main/java/za/ac/wits/cpd/service/twitconpro/impl/FileHelper.java
+++ twitter/java-data-extraction-service/src/main/java/za/ac/wits/cpd/service/twitconpro/impl/FileHelper.java
@@ -4,6 +4,11 @@ package za.ac.wits.cpd.service.twitconpro.impl;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.logging.Level;
 import javax.ejb.Stateless;
 import lombok.extern.java.Log;
@@ -46,6 +51,44 @@ public class FileHelper {
         }
     }
     
+    public void write(URI uri, String tweet){
+        try {
+            File file = new File(uri.toString() + toDefaultFileName());
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write(tweet);
+                log.log(Level.OFF, "{0} written....", file.getAbsolutePath());
+            }
+        } catch (IOException ex) {
+            log.log(Level.SEVERE, "Failed to write a tweet to file", ex);
+        }finally{
+            
+        }
+    }
+    
+    public void write(URI uri,String fileName, String tweet){
+        try {
+            File file = new File(uri.toString() + fileName);
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write(tweet);
+                log.log(Level.OFF, "{0} written....", file.getAbsolutePath());
+            }
+        } catch (IOException ex) {
+            log.log(Level.SEVERE, "Failed to write a tweet to file", ex);
+        }finally{
+            
+        }
+    }
+    
+    private String generateTimestamp() {
+        DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd_hh_mm_ss");
+        return fmt.format(new Date());
+    }
+    
+    private String toDefaultFileName(){
+        return generateTimestamp()+DOT_JSON;
+    }
+    
     private static final String DOT_JSON = ".json";
     private static final String FILE_NAME_PREFIX = "tweets/tweet-";
+
 }
